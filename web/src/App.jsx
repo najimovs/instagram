0344bac004d319f93e7b0a442a683bb754ef2397
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-09-03 10:15:40 +0500

    Add router

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css"
+import { Routes, Route } from "react-router"
 
 export function App() {
 
-	return <>App</>
+	return <>
+		<Routes>
+			<Route path="/" element={ <>Home</> } end />
+		</Routes>
+	</>
 }
