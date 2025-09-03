72572494c5313fe8fd821e550f15f8c500143f09
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-09-03 10:10:37 +0500

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,23 @@
+/* WEB */
+
+/*
+	GLOBAL
+	insta.com/join - Signup
+	insta.com/login - Login
+	insta.com/reset - Password reset
+	insta.com/aisha - User's homepage
+	insta.com/about - About project
+
+	USER
+	insta.com/settings - My settings
+	insta.com/post - Add new post
+	insta.com/profile - My profile
+	insta.com/notifications - Likes, Comments
+
+	Homepage
+	insta.com
+		if logged in:
+			Reels
+		otherwise:
+			Global landing page
+*/
