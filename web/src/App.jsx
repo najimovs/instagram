77f70c021536fe18896d13f025424af84ab8685b
firdavsdev07 <firdavsnormurodov435@gmail.com>
2025-09-06 09:20:13 +0500

    language-select ui

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,26 +1,27 @@
-import "./App.css"
-import { useContext, useState } from "react"
-import { Routes, Route } from "react-router"
+import "./App.css";
+import { useContext, useState } from "react";
+import { Routes, Route } from "react-router";
 
 // Import contexts
-import { LanguageContext } from "./Contexts/LanguageContext"
-import { UserContext } from "./Contexts/UserContext"
+import { LanguageContext } from "./Contexts/LanguageContext";
+import { UserContext } from "./Contexts/UserContext";
 
 // Import pages
-import { Home } from "./Pages/Home/Home"
+import { Home } from "./Pages/Home/Home";
 
 export function App() {
+  const [language, setLanguage] = useState("english");
+  const [username, setUsername] = useState(null);
 
-	const [ language, setLanguage ] = useState( "english" )
-	const [ username, setUsername ] = useState( null )
-
-	return <>
-		<LanguageContext.Provider value={ [ language, setLanguage ] }>
-			<UserContext.Provider value={ [ username, setUsername ] }>
-				<Routes>
-					<Route path="/" element={ <Home /> } end />
-				</Routes>
-			</UserContext.Provider>
-		</LanguageContext.Provider>
-	</>
+  return (
+    <>
+      <LanguageContext.Provider value={[language, setLanguage]}>
+        <UserContext.Provider value={[username, setUsername]}>
+          <Routes>
+            <Route path="/" element={<Home />} end />
+          </Routes>
+        </UserContext.Provider>
+      </LanguageContext.Provider>
+    </>
+  );
 }
--- web/src/Components/Footer/Footer.jsx
+++ web/src/Components/Footer/Footer.jsx
@@ -1,27 +1,46 @@
+import { useState, useContext } from "react";
+import { LanguageContext } from "../../Contexts/LanguageContext";
 export function Footer() {
-
-	return (
-		<footer>
-			<section>
-				<ul className="links">
-					<li>
-						<a href="/">About</a>
-					</li>
-					<li>
-						<a href="/">Login</a>
-					</li>
-					<li>
-						<a href="/">Join</a>
-					</li>
-				</ul>
-				<p>Copyright &copy; App 2025. All rights reserved.</p>
-			</section>
-			<section>
-				<div className="tools">
-					<button>Language</button>
-					<button>Light mode</button>
-				</div>
-			</section>
-		</footer>
-	)
+  const [open, setOpen] = useState(false);
+  function handleSelect(e) {
+    setLanguage(e.target.getAttribute("value"));
+    setOpen(false);
+  }
+  const [language, setLanguage] = useContext(LanguageContext);
+  return (
+    <footer>
+      <section>
+        <ul className="links">
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+          <li>
+            <a href="/">Join</a>
+          </li>
+        </ul>
+        <p>Copyright &copy; App 2025. All rights reserved.</p>
+      </section>
+      <section>
+        <div className="tools">
+          <div className="dropdown">
+            <div className={`dropdown-menu ${open ? "show" : ""}`}>
+              <div value="english" onClick={(e) => handleSelect(e)}>
+                english
+              </div>
+              <div value="uzbek" onClick={(e) => handleSelect(e)}>
+                uzbek
+              </div>
+            </div>
+            <button onClick={() => setOpen(!open)} id="langBtn">
+              {language}
+            </button>
+          </div>
+          <button>Light mode</button>
+        </div>
+      </section>
+    </footer>
+  );
 }
